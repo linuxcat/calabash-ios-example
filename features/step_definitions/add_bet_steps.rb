cd545c0a4c66be16015c7c815aa4a6bddc87bdf3
linuxcat <sarndeepnijjar@googlemail.com>
2013-08-08 15:09:30 +0100

    SN: another one

--- features/step_definitions/add_bet_steps.rb
+++ features/step_definitions/add_bet_steps.rb
@@ -6,8 +6,7 @@ end
 
 Then(/^the book opens and I am taken to the bet page$/) do
   bet.is_bet_screen?.should == true
-  @current_bets = bet.get_no_of_bets
-  puts @current_bets
+  puts "my current bets are:#{@current_bets}"
 end
 
 When(/^I add a new bet for \$(\d+) and click the save button$/) do |arg|
@@ -16,6 +15,5 @@ end
 
 Then(/^The bet is added to my bet book$/) do
   bet.is_bet_screen?.should == true
-  puts bet.get_no_of_bets
   bet.get_no_of_bets.should == @current_bets  + 1
 end
