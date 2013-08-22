3ee4093b4d5fac329298a54d61857b6ca891ba86
linuxcat <sarndeepnijjar@googlemail.com>
2013-08-22 20:32:21 +0100

    SN: added clear form text function

--- core/helpers/ios_helper.rb
+++ core/helpers/ios_helper.rb
@@ -6,21 +6,11 @@ module IosHelper
     include Calabash::Cucumber::Operations
 
 
-
-
-
   def clear_form_text
     string_length = query('TextView', 'text').first.size
     string_length.times do
       keyboard_enter_char 'Delete'
     end
   end
-
-  def delete_characters(string_length)
-    string_length.times do
-      keyboard_enter_char 'Delete'
-    end
-  end
-
 
 end
